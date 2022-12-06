f4223ba2dd57f8d3255a1a528ba2ba7436145b1b
lhamro12 <117490435+lhamro12@users.noreply.github.com>
2022-12-05 21:35:41 -0500

    Delete Django_website directory

--- Django_website/mcu/home/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-# Create your views here.
-# Note: The stuff above this will already be in this file.
-from django.http import HttpResponse
-
-def index(request):
-    return render(request, 'home.html')
